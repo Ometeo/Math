1ea46ae1571bd7f71fb836b73ce003f8d0161eb4
Jonathan Bihet <jonathan.bihet@gmail.com>
2014-01-01 22:05:58 +0100

    [add] Debut de sutherland-hodgman
    - Implmentation algo suther hod
    - Ajout des signature des methodes pour l'algo
    - Stockage des normales dans un vector de struct vec
    - separation de l'affichage et du stocakges des normales
    - bonus : appui sur n permet d'afficher ou non les normales

--- TPMath/main.cpp
+++ TPMath/main.cpp
@@ -10,12 +10,24 @@ using namespace std;
 
 double c;
 bool drawPoly;
+bool drawingNormals;
+
+
+struct vec {
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+};
+
 
 
 vector<POINT> windowsPoints;
 vector<POINT> polygonPoints;
-vector<POINT> normals;
 
+vector<POINT> polygonPointsWindowed;
+
+vector<vec> windowsNormals;
 
 void init(int argc, char **argv);
 bool isClockwise(vector<POINT>);
@@ -28,15 +40,33 @@ bool findPoint(int x, int y, vector<POINT> pointVector, POINT& p);
 float VectorNorm(float x1, float x2, float y1, float y2);
 
 float DotProduct(float x1, float x2, float y1, float y2);
+void drawNormals(vector<vec> normals);
+
+
+//Algo sutherland-Hodgman
+void SutherlandHodgman(vector<POINT> polygonPoints, vector<POINT> windowsPoints);
+
+
+//Retourne un booléen suivant l'intersection possible entre le côté [currentPolyPoint otherPolyPoint] du polygone et le bord prolongé (une droite) (windowPoint1 windowPoint2) de la fenêtre.
+bool cut(POINT currentPolyPoint, POINT otherPolyPoint, POINT windowPoint1, POINT windowPoint2);
+
+//retournant le point d'intersection [[currentPolyPoint otherPolyPoint] inter (windowPoint1 windowPoint2)
+POINT intersect(POINT currentPolyPoint, POINT otherPolyPoint, POINT windowPoint1, POINT windowPoint2);
+
+//Retourne un booléen si S est visible par rapport à (windowPoint1 windowPoint2
+bool visible(POINT currentPolyPoint, POINT windowPoint1, POINT windowPoint2);
+
+
+
 
 int main (int argc, char **argv)
 {
-    drawPoly = true;
+
     init(argc, argv);
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
-	glutMouseFunc(mouse);
+    glutMouseFunc(mouse);
 
     glutMainLoop();
     return 0;
@@ -55,6 +85,9 @@ void init(int argc, char **argv)
     glClearColor(0.0,0.0,0.0,0.0);
     glColor3f(1.0,1.0,1.0);
     glPointSize(2.0);
+
+    drawPoly = true;
+    drawingNormals = false;
 }
 
 /**
@@ -63,7 +96,7 @@ void init(int argc, char **argv)
 bool isClockwise(vector<POINT> poly)
 {
     int sum = 0;
-    for(int i = 0; i < poly.size(); i++)
+    for(unsigned int i = 0; i < poly.size(); i++)
     {
         int x1 = poly[i].x;
         int y1 = poly[i].y;
@@ -84,16 +117,16 @@ void display()
 
     if(windowsPoints.size() > 1)
     {
-        for(int i = 1; i < windowsPoints.size(); i++)
+        for(unsigned int i = 1; i < windowsPoints.size(); i++)
         {
-                glBegin(GL_LINES);
-                    glVertex2i(windowsPoints[i-1].x, windowsPoints[i-1].y);
-                    glVertex2i(windowsPoints[i].x, windowsPoints[i].y);
-                glEnd();
+            glBegin(GL_LINES);
+            glVertex2i(windowsPoints[i-1].x, windowsPoints[i-1].y);
+            glVertex2i(windowsPoints[i].x, windowsPoints[i].y);
+            glEnd();
         }
         glBegin(GL_LINES);
-            glVertex2i(windowsPoints[windowsPoints.size()-1].x, windowsPoints[windowsPoints.size()-1].y);
-            glVertex2i(windowsPoints[0].x, windowsPoints[0].y);
+        glVertex2i(windowsPoints[windowsPoints.size()-1].x, windowsPoints[windowsPoints.size()-1].y);
+        glVertex2i(windowsPoints[0].x, windowsPoints[0].y);
         glEnd();
     }
 
@@ -102,22 +135,19 @@ void display()
     if(polygonPoints.size() > 1)
     {
         POINT pt1, pt2;
-        for(int i = 0; i < polygonPoints.size(); i++)
+        for(unsigned int i = 0; i < polygonPoints.size(); i++)
         {
             pt1 = polygonPoints[i];
             pt2 = polygonPoints[(i + 1) % polygonPoints.size()];
             glBegin(GL_LINES);
-                glVertex2i(pt1.x, pt1.y);
-                glVertex2i(pt2.x, pt2.y);
+            glVertex2i(pt1.x, pt1.y);
+            glVertex2i(pt2.x, pt2.y);
             glEnd();
 
         }
     }
-
-    if(windowsPoints.size() > 2)
-    {
-        VectorNormal(windowsPoints);
-    }
+    if(drawingNormals)
+        drawNormals(windowsNormals);
 
     cout << "polygon isClockwise : " << isClockwise(windowsPoints) << endl;
 
@@ -126,19 +156,26 @@ void display()
 
 void mouse(int button,int state,int x,int y)
 {
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-	{
-		POINT temp;
-		temp.x = x - 250;
-		temp.y = -y + 250;
-		if(drawPoly)
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        POINT temp;
+        temp.x = x - 250;
+        temp.y = -y + 250;
+        if(drawPoly)
             polygonPoints.push_back(temp);
         else
+        {
             windowsPoints.push_back(temp);
-		display();
-	}
+            if(windowsPoints.size() > 2)
+            {
+                VectorNormal(windowsPoints);
+            }
+        }
 
-	if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+        display();
+    }
+
+    if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
     {
         if(drawPoly)
         {
@@ -153,14 +190,21 @@ void mouse(int button,int state,int x,int y)
     }
 }
 
-void keyboard(unsigned char touche,int x,int y){
-	switch (touche){
-        case 'w':
-            drawPoly = false;
-            break;
-        case 'p':
-            drawPoly = true;
-            break;
+void keyboard(unsigned char touche,int x,int y)
+{
+    switch (touche)
+    {
+    case 'w':
+        drawPoly = false;
+        break;
+    case 'p':
+        drawPoly = true;
+        break;
+    case 'n':
+        drawingNormals = !drawingNormals;
+        display();
+        break;
+
 
 //		case 'p':/* affichage du carré plein*/
 //			glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
@@ -177,22 +221,38 @@ void keyboard(unsigned char touche,int x,int y){
 //			glutPostRedisplay();
 //			break;
 
-		case 'q':/* Quitter le programme */
-			exit(0);
-	}
+    case 'q':/* Quitter le programme */
+        exit(0);
+    }
 }
 
 
 void drawPoints(vector<POINT> pointVector)
 {
     glBegin(GL_POINTS);
-    for(int i = 0; i < pointVector.size(); i++)
+    for(unsigned int i = 0; i < pointVector.size(); i++)
     {
         glVertex2i(pointVector[i].x, pointVector[i].y);
     }
     glEnd();
 }
 
+void drawNormals(vector<vec> normals)
+{
+    glColor3f(1.0,1.0,0.0);
+    for(unsigned int i = 0; i < normals.size(); i++)
+    {
+        glBegin(GL_POINTS);
+        glVertex2i(normals[i].x1,normals[i].y1);
+        glVertex2i(normals[i].x2, normals[i].y2);
+        glEnd();
+
+        glBegin(GL_LINES);
+        glVertex2i(normals[i].x1,normals[i].y1);
+        glVertex2i(normals[i].x2,normals[i].y2);
+        glEnd();
+    }
+}
 
 
 bool findPoint(int x, int y, vector<POINT> pointVector, POINT& p)
@@ -202,62 +262,46 @@ bool findPoint(int x, int y, vector<POINT> pointVector, POINT& p)
 
 void VectorNormal(vector<POINT> polygonPoints)
 {
-        bool clockwise = isClockwise(polygonPoints);
-        float X, Y, distance, distanceX, distanceY, vectorNormalY, vectorNormalX;
+    bool clockwise = isClockwise(polygonPoints);
+    float X, Y, vectorNormalY, vectorNormalX;
 
-        float norm;
+    float norm;
 
-        POINT pt1, pt2;
+    POINT pt1, pt2;
 
-        for(int i = 0; i < polygonPoints.size(); i++)
+    windowsNormals.clear();
+
+    for(unsigned int i = 0; i < polygonPoints.size(); i++)
+    {
+        pt1 = polygonPoints[i];
+        pt2 = polygonPoints[(i + 1) % polygonPoints.size()];
+
+        Y = pt1.y + ((pt2.y)-(pt1.y))*0.5;
+        X = pt1.x + ((pt2.x)-(pt1.x))*0.5;
+
+        if(clockwise)
         {
-            pt1 = polygonPoints[i];
-            pt2 = polygonPoints[(i + 1) % polygonPoints.size()];
-
-            Y = pt1.y + ((pt2.y)-(pt1.y))*0.5;
-            X = pt1.x + ((pt2.x)-(pt1.x))*0.5;
-
-            if(clockwise)
-            {
-                vectorNormalX = ((pt2.y)-(pt1.y));
-                vectorNormalY = (-((pt2.x)-(pt1.x)));
-            }
-            else
-            {
-                vectorNormalX = (-((pt2.y)-(pt1.y)));
-                vectorNormalY = ((pt2.x)-(pt1.x));
-            }
-
-            norm = VectorNorm(vectorNormalX, X, vectorNormalY, Y);
-            cout << norm << endl;
-
-            vectorNormalX /= norm;
-            vectorNormalY /= norm;
-
-            vectorNormalX *= 10;
-            vectorNormalY *= 10;
-
-            glColor3f(1.0,0.0,0.0);
-            glBegin(GL_POINTS);
-            glVertex2i(X,Y);
-            glVertex2i(X+vectorNormalX, Y+vectorNormalY);
-            glEnd();
-
-            glBegin(GL_LINES);
-                glVertex2i(X,Y);
-                glVertex2i(X+vectorNormalX, Y+vectorNormalY);
-            glEnd();
-
-           /* glColor3f(1.0,0.0,0.0);
-            glBegin(GL_POINTS);
-            glVertex2i(X,Y);
-            glVertex2i(X-vectorNormalX, Y-vectorNormalY);
-            glEnd();
-            glBegin(GL_LINES);
-                glVertex2i(X,Y);
-                glVertex2i(X-vectorNormalX, Y-vectorNormalY);
-           glEnd();*/
+            vectorNormalX = ((pt2.y)-(pt1.y));
+            vectorNormalY = (-((pt2.x)-(pt1.x)));
         }
+        else
+        {
+            vectorNormalX = (-((pt2.y)-(pt1.y)));
+            vectorNormalY = ((pt2.x)-(pt1.x));
+        }
+
+        norm = VectorNorm(vectorNormalX, X, vectorNormalY, Y);
+        vectorNormalX /= norm/10;
+        vectorNormalY /= norm/10;
+
+        vec temp;
+        temp.x1 = X;
+        temp.y1 = Y;
+        temp.x2 = X + vectorNormalX;
+        temp.y2 = Y + vectorNormalY;
+
+        windowsNormals.push_back(temp);
+    }
 }
 
 
@@ -284,3 +328,68 @@ float VectorNorm(float x1, float x2, float y1, float y2)
 {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
+
+//Algo sutherland-Hodgman
+void SutherlandHodgman(vector<POINT> polygonPoints, vector<POINT> windowsPoints)
+{
+    vector<POINT> PS;
+
+
+    POINT F;
+    POINT S;
+    POINT I;
+    for(unsigned int i = 0; i < windowsPoints.size(); i++)
+    {
+        PS.clear();
+        for(unsigned int j = 0; j < polygonPoints.size() - 1; j++)
+        {
+            if(j == 0)
+            {
+                F = polygonPoints[j];
+            }
+            else
+            {
+                if(cut(S,polygonPoints[j], windowsPoints[i], windowsPoints[(i + 1) % windowsPoints.size()]))
+                {
+                    I = intersect(S, polygonPoints[j], windowsPoints[i], windowsPoints[(i + 1) % windowsPoints.size()]);
+                    PS.push_back(I);
+                }
+            }
+            S = polygonPoints[j];
+            if(visible(S, windowsPoints[i], windowsPoints[(i + 1) % windowsPoints.size()]))
+            {
+                PS.push_back(S);
+            }
+        }
+        if(PS.size() > 0)
+        {
+            if(cut(S, F, windowsPoints[i], windowsPoints[(i + 1) % windowsPoints.size()]))
+            {
+                I = intersect(S, F, windowsPoints[i], windowsPoints[(i + 1) % windowsPoints.size()]);
+                PS.push_back(I);
+            }
+            polygonPointsWindowed = PS;
+        }
+    }
+}
+
+//Retourne un booléen suivant l'intersection possible entre le côté [currentPolyPoint otherPolyPoint] du polygone et le bord prolongé (une droite) (windowPoint1 windowPoint2) de la fenêtre.
+bool cut(POINT currentPolyPoint, POINT otherPolyPoint, POINT windowPoint1, POINT windowPoint2)
+{
+    return false;
+}
+
+//retournant le point d'intersection [[currentPolyPoint otherPolyPoint] inter (windowPoint1 windowPoint2)
+POINT intersect(POINT currentPolyPoint, POINT otherPolyPoint, POINT windowPoint1, POINT windowPoint2)
+{
+    POINT p;
+    return p;
+}
+
+//Retourne un booléen si S est visible par rapport à (windowPoint1 windowPoint2
+bool visible(POINT currentPolyPoint, POINT windowPoint1, POINT windowPoint2)
+{
+    return false;
+}
+
+
